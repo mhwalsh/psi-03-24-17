fca73d03a6bec80c15f433bb8739bcfa8bfda2ec
Millie Walsh <millie@primeacademy.io>
2017-03-24 13:10:31 -0500

    live chat today code

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('test');
+
+// concatoaienation ?
+// addition for strings
+var aString = 7587;
+console.log("Dev and Millie " + aString);
+
+// conditional statments
+// if / else if / else
+// string? a number? or anything else
+
+var ourThing = "alsdkfjaied";
+console.log(typeof ourThing);
+
+if(typeof ourThing === "string"){ //yes or no true or false
+  console.log("It's a string!");
+}else if(typeof ourThing === "number"){
+  console.log("Im a number!");
+}else{ // otherwise or anything else
+  console.log("I don't know the type, but it isn't a string for sure!");
+}
